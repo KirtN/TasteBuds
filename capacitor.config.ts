cc0e80fabe850f7f5779a021e03bae7256fcca27
Josh Phang <jphang@sfu.ca>
2024-10-24 18:38:39 -0700

    Created Final Project: TasteBuds

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TasteBuds',
+  webDir: 'dist/taste-buds/browser'
+};
+
+export default config;
