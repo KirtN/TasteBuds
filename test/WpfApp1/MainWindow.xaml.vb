f3f68f73fc0dc2034f7bedf9d2651bbdb890652f
KirtN <101079107+KirtN@users.noreply.github.com>
2024-11-15 19:01:54 -0800

    Add files via upload

---
+++ test/WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,7 @@
+﻿Class MainWindow
+
+    Private Sub StartSimulationButton_Click(sender As Object, e As RoutedEventArgs)
+        ' Your simulation logic here
+        MessageBox.Show("Simulation started!")
+    End Sub
+End Class
